a4c7cd188ac1468cbe9a59cbbf3c354efdfe1aea
wkpids@gmail.com <kpids@pop-os.localdomain>
2022-08-30 15:26:07 +0300

    1.0.2 release

--- src-tauri/crates/discord-rich-presence/src/discord_ipc_mutex.rs
+++ src-tauri/crates/discord-rich-presence/src/discord_ipc_mutex.rs
@@ -1,10 +1,7 @@
+// use std::sync::mpsc::{self, Sender, Receiver, channel};
 use std::sync::{Arc, Mutex};
-
 use tokio::time::{interval,  Duration};
-
-use crate::DiscordIpcClient;
-
-use crate::DiscordIpc;
+use crate::{DiscordIpc, DiscordIpcClient};
 
 #[derive(Debug)]
 pub struct DiscordIpcClientMutex (pub Arc<Mutex<DiscordIpcClient>>);
@@ -12,15 +9,22 @@ pub struct DiscordIpcClientMutex (pub Arc<Mutex<DiscordIpcClient>>);
 impl DiscordIpcClientMutex {
 
     pub fn new(client_id: &str) -> DiscordIpcClientMutex {
-        DiscordIpcClientMutex(
+
+        let client = DiscordIpcClientMutex(
             Arc::new(
                 Mutex::new(
                     DiscordIpcClient::new(client_id)
                     .unwrap()
                 )   
             )
-        )
+        );
+
+        client.start_loop();
+
+        client
+
     }
+
 
     pub fn enable(&self) {
 
@@ -29,6 +33,18 @@ impl DiscordIpcClientMutex {
         if lock.enabled { return }
 
         lock.enabled = true;
+    }
+
+    pub fn disable(&self) {
+
+        let mut lock = self.0.lock().unwrap();
+
+        if !lock.enabled { return }
+
+        lock.enabled = false;
+    }
+
+    fn start_loop(&self) {
 
         let mut interval = interval(Duration::from_secs(1));
 
@@ -39,28 +55,78 @@ impl DiscordIpcClientMutex {
             interval.tick().await;
 
             let mut lock = clone.lock().unwrap();
-            
-            // println!("{:?}", lock);
 
             if !lock.enabled {
                 if lock.connected && lock.close().is_ok() { println!("Closed Discord IPC client...") }
-                break;
+                continue;
             }
 
             if lock.connected { continue };
 
-            println!("Trying to connect..");
+            // println!("Trying to connect..");
 
             if lock.connect().is_ok() { println!("Connected to Discord IPC client!") }
 
         }});
-
     }
 
-    pub fn disable(&self) {
-
-        let mut lock = self.0.lock().unwrap();
-
-        lock.enabled = false;
-    }
 }
+
+// #[derive(Debug)]
+// pub struct DiscordIpcClientMutex (pub Arc<Mutex<DiscordIpcClient>>);
+
+// impl DiscordIpcClientMutex {
+
+//     pub fn new(client_id: &str) -> DiscordIpcClientMutex {
+//         DiscordIpcClientMutex(
+//             Arc::new(
+//                 Mutex::new(
+//                     DiscordIpcClient::new(client_id)
+//                     .unwrap()
+//                 )   
+//             )
+//         )
+//     }
+
+//     pub fn enable(&self) {
+
+//         let mut lock = self.0.lock().unwrap();
+
+//         if lock.enabled { return }
+
+//         lock.enabled = true;
+
+//         let mut interval = interval(Duration::from_secs(1));
+
+//         let clone = Arc::clone(&self.0);
+
+//         tokio::spawn(async move { loop {
+
+//             interval.tick().await;
+
+//             let mut lock = clone.lock().unwrap();
+            
+//             // println!("{:?}", lock);
+
+//             if !lock.enabled {
+//                 if lock.connected && lock.close().is_ok() { println!("Closed Discord IPC client...") }
+//                 break;
+//             }
+
+//             if lock.connected { continue };
+
+//             println!("Trying to connect..");
+
+//             if lock.connect().is_ok() { println!("Connected to Discord IPC client!") }
+
+//         }});
+
+//     }
+
+//     pub fn disable(&self) {
+
+//         let mut lock = self.0.lock().unwrap();
+
+//         lock.enabled = false;
+//     }
+// }
